19fdfb490284c7cb2d62103f818f7fdb03a0a0c8
Deborah Kurz <DeborahKurz6@gmail.com>
2024-10-03 11:17:23 -0600

    Complete Lab

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,45 @@
-class Stack:
+#impliment a stack, use a list as the underlying data structure
+#Call on built-in list methods to build your Stack class' functionality
+#Use search() method
 
-    def __init__(self, items = [], limit = 100):
-        pass
+class Stack:
+    def __init__(self, items=None, limit=100):
+        if items is None:
+            self.items = []
+        else:
+            self.items = list(items)
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if len(self.items) < self.limit:
+            self.items.append(item)
 
     def pop(self):
-        pass
+        if len(self.items) > 0:
+            item = self.items.pop()
+            return item
 
     def peek(self):
         pass
     
     def size(self):
-        pass
+        length = len(self.items)
+        return length
 
     def full(self):
-        pass
+        if len(self.items) == self.limit:
+            return True
+        else: 
+            return False
 
     def search(self, target):
-        pass
+        try:
+            index = self.items.index(target)
+            print(target, index)
+            distance = len(self.items) - index -1
+            return distance
+        except ValueError:
+            return -1
